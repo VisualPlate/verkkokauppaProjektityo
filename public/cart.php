6ff7786cf4d4d3c1271e138abc5401c5fe60db7c
Jere Tolonen <tolonenjere0@gmail.com>
2025-11-26 15:04:09 +0200

    update

--- public/cart.php
+++ public/cart.php
@@ -7,32 +7,26 @@ require_once '../backend/helpers/password_helper.php'; // Salasanan apufunktiot
 include "header_footer/header.php";  // Include header
 ?>
 
-<html lang="en">
-<head>
- <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ruoka</title>
-    <link rel="stylesheet" href="assets/css/root.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/cart.css">
-</head>
-<body>
-    
 <!DOCTYPE html>
 <html lang="fi">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ostoskori</title>
+    <link rel="stylesheet" href="assets/css/root.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/cart.css">
 </head>
 <body>
     <div class="container">
         <div class="header">
             <h1>Ostoskori</h1>
         </div>
-
         <div class="cart-box">
-            <div id="cart-items"></div>
+            <div id="cart-items">
+
+            <!--cart items here-->
+            </div>
 
             <div class="summary">
                 <div class="summary-row">
@@ -45,22 +39,8 @@ include "header_footer/header.php";  // Include header
                     <span>Yhteensä</span>
                 </div>
             </div>
-
             <button class="checkout-btn">Maksa</button>
         </div>
     </div>
-
-   
 </body>
-</html>
-
-
-
-
-
-
-</body>
-
-
-
 </html>
