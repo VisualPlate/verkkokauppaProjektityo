051705678861fb59107b756c226c154f62de9e02
Justus Putkonen <mattinukku771@gmail.com>
2025-12-01 08:44:32 +0200

    Add files via upload

--- public/items.php
+++ public/items.php
@@ -28,46 +28,42 @@ include "header_footer/header.php";  // Include header
                 <a href="categories.php" class="check-btn" style="width:120px">Enemmän</a>
             </div>
             <div class="categoryRow row top-margin">
-                <?php
-                // Fetch from API
-                $url = "http://localhost/verkkokauppaProjektityo/backend/api/products/read.php";
-
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                curl_setopt($ch, CURLOPT_URL, $url);
-
-                $result = curl_exec($ch);
-                curl_close($ch);
-
-                // Decode JSON to associative array
-                $data = json_decode($result, true);
-
-                // Check if decoding worked
-                if (!$data) {
-                    echo "Invalid JSON or empty response";
-                    exit;
-                }
-
-                // Loops each item and show them to user
-                foreach ($data as $item):?>
-                    <a href="item.php?productId=<?=$item["productID"]?>" style="all:unset;cursor:pointer">
-                        <div class="col space-between box">
-                            <div id="row-items">
-                                <img src="assets/<?=$item["imagePath"]?>" alt="Kuva" class="product-img">
-                                <div class="row space-between nav-align">
-                                    <h2><?=$item["name"]?></h2>
-                                    <h4><?=$item["price"]?></h4>
-                                </div>
-                            </div>
-                        
-                        <div class="row space-between">
-                            <form method="post">
-                                <button class="check-btn">Lisää ostoskoriin</button>
-                            </form>
-                        </div></a>
+                <div class="col space-between box">
+                    <div id="row-items">
+                        <img src="assets/images/background/green.svg" alt="Kuva" class="product-img">
+                        <div class="row space-between nav-align">
+                            <h2>Makkara</h2>
+                            <h4>12.99</h4>
+                        </div>
                     </div>
-                <?php endforeach
-                ?>
+                    <div class="row space-between">
+                        <button class="check-btn">Lisää ostoskoriin</button>
+                    </div>
+                </div>
+                <div class="col space-between box">
+                    <div id="row-items">
+                        <img src="assets/images/background/green.svg" alt="Kuva" class="product-img">
+                        <div class="row space-between nav-align">
+                            <h2>Makkara</h2>
+                            <h4>12.99</h4>
+                        </div>
+                    </div>
+                    <div class="row space-between">
+                        <button class="check-btn">Lisää ostoskoriin</button>
+                    </div>
+                </div>
+                <div class="col space-between box">
+                    <div id="row-items">
+                        <img src="assets/images/background/green.svg" alt="Kuva" class="product-img">
+                        <div class="row space-between nav-align">
+                            <h2>Makkara</h2>
+                            <h4>12.99</h4>
+                        </div>
+                    </div>
+                    <div class="row space-between">
+                        <button class="check-btn">Lisää ostoskoriin</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
